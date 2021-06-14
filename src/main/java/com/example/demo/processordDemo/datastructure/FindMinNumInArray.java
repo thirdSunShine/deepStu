4c644ff7f690fe74173e4f3cfbdce5fd9f00298a
Administrator <thirdsunshine@163.com>
2021-06-14 21:45:38 +0800

    排序训练

---
+++ src/main/java/com/example/demo/processordDemo/datastructure/FindMinNumInArray.java
@@ -0,0 +1,28 @@
+package com.example.demo.processordDemo.datastructure;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class FindMinNumInArray {
+    public static void main(String[] args) {
+        Integer[] arrays = new Integer[10];
+        for (int i = 0 ; i < 10; i++){
+            arrays[i] = new Random().nextInt(10);
+        }
+        int min = findMinNumInArray(arrays);
+        System.out.println(min);
+    }
+
+    private static int findMinNumInArray(Integer[] arrays) {
+        List<Integer> ints = Arrays.asList(arrays);
+        System.out.println("original array:" + ints);
+        int temp = arrays[0];
+        for (int i = 1 ;i<arrays.length;i++){
+            if (arrays[i] < temp){
+                temp = arrays[i];
+            }
+        }
+        return temp;
+    }
+}
